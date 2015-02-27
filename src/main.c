974c70283299df691db2a4c5ca0d65cca89bd77c
Parham Alvani <parham.alvani@gmail.com>
2015-02-27 23:39:11 +0330

    main is added.

---
+++ src/main.c
@@ -0,0 +1,20 @@
+/*
+ * In The Name Of God
+ * ========================================
+ * [] File Name : main.c
+ *
+ * [] Creation Date : 27-02-2015
+ *
+ * [] Last Modified : Fri 27 Feb 2015 11:38:47 PM IRST
+ *
+ * [] Created By : Parham Alvani (parham.alvani@gmail.com)
+ * =======================================
+*/
+#include <stdio.h>
+
+#include "cache.h"
+
+int main(int argc, char *argv[])
+{
+
+}
