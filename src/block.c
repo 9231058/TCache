8ec1cd8642e8a5c765a1fb3d619113e2147988d1
Parham Alvani <parham.alvani@gmail.com>
2015-02-27 22:11:37 +0330

    primary object improved.

--- src/block.c
+++ src/block.c
@@ -5,7 +5,7 @@
  *
  * [] Creation Date : 27-02-2015
  *
- * [] Last Modified : Fri 27 Feb 2015 09:39:53 PM IRST
+ * [] Last Modified : Fri 27 Feb 2015 10:06:45 PM IRST
  *
  * [] Created By : Parham Alvani (parham.alvani@gmail.com)
  * =======================================
@@ -15,10 +15,12 @@
 
 #include "block.h"
 
-struct block *block_new(uint64_t tag, uint64_t set)
+struct block *block_new(void)
 {
 	struct block *new = malloc(sizeof(struct block));
-	new->tag = tag;
+	
+	new->valid = 0;
+	new->access = 0;
 	new->next = NULL;
 	return new;
 }
