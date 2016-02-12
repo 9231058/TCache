e64b9219f0e0da7d134a6a35d160e5d14990997b
Parham Alvani <parham.alvani@gmail.com>
2016-02-12 19:22:13 +0330

    Some comments were added to cache_ctl.

--- src/cache_ctl.h
+++ src/cache_ctl.h
@@ -18,23 +18,38 @@
 
 #include "set.h"
 
+/*
+ * Replacing function callback
+*/
 void set_replace_function(void (*new_replace)(
 	struct set *set, uint64_t tag, uint64_t index));
 
 void use_replace_function(struct set *set, uint64_t tag, uint64_t index);
 
+/*
+ * On Replace function callback
+*/
 void set_on_replace_function(void (*new_replace)(uint64_t tag, uint64_t index));
 
 void use_on_replace_function(uint64_t tag, uint64_t index);
 
+/*
+ * On Hit function callback
+*/
 void set_on_hit_function(void (*new_hit)(uint64_t tag, uint64_t index));
 
 void use_on_hit_function(uint64_t tag, uint64_t index);
 
+/*
+ * On Miss function callback
+*/
 void set_on_miss_function(void (*new_miss)(uint64_t tag, uint64_t index));
 
 void use_on_miss_function(uint64_t tag, uint64_t index);
 
+/*
+ * Set block size function callback
+*/
 void set_block_size(uint64_t block_size);
 
 void use_block_size(uint64_t * address);
